4ecf14eed6493d1ca28e25139173dbaa0b215cea
Krijf <BenediktGraf@outlook.de>
2021-06-20 09:31:56 +0200

    PID Bed konfiguriert

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -563,9 +563,9 @@
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
   // Ender 3 V2
-  #define DEFAULT_bedKp 462.10
-  #define DEFAULT_bedKi  85.47
-  #define DEFAULT_bedKd 624.59
+  #define DEFAULT_bedKp 123.91
+  #define DEFAULT_bedKi  20.89
+  #define DEFAULT_bedKd 490.08
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
